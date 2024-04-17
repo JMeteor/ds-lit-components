649c00168a5bcfa30edb88a2ad81472dd9659cac
JMeteor <jerzy.zientek@gmail.com>
2024-04-17 21:57:59 +0200

    Fix variables import in storybook preview

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   build: {
     lib: {
@@ -12,6 +13,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      // @ts-ignore
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
